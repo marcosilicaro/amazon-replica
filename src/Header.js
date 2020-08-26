6c173e5d2e9013b45b811f0679510136c0d051ec
marcosilicaro <marcosilicaro@gmail.com>
2020-08-26 14:19:40 -0300

    header

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import './Header.css'
 
 function Header() {
   return (
-  <div>
+  <div class='header__navbar'>
+    <div className='header__logoMenu'>
+      <Link to='/'>Menu</Link>
+      <Link to='/'>Logo</Link>
+    </div>
+    <div className='header__searchBar'>
+      Search Bar
+    </div>
+    <div className='header__rightOptions'>
       <Link to='/'>Home</Link>
-      <Link to='/checkout'>Checkout</Link>
-      <Link to='/login'>Login</Link>
+    </div>
   </div>
   );
 }
