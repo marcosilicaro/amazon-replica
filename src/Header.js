ed8a96190a85e34ab39a8452b4d9f537b00bd60e
Marco Israel Silicaro <marcoisraelsilicaro@Marcos-MacBook-Pro.local>
2020-08-28 13:16:52 -0300

    header styling

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import SearchIcon from '@material-ui/icons/Search';
 import './Header.css'
 
 function Header() {
   return (
   <div class='header__navbar'>
     <div className='header__logoMenu'>
-      <Link to='/'>Menu</Link>
-      <Link to='/'>Logo</Link>
+      <Link to='/'>
+        <img 
+        className='header__logo'
+        alt='amazon-logo'
+        src='https://pngimg.com/uploads/amazon/amazon_PNG25.png'></img>
+      </Link>
     </div>
     <div className='header__searchBar'>
-      Search Bar
+      <form>
+      <input type="text" />
+      <SearchIcon/>
+      </form>
     </div>
     <div className='header__rightOptions'>
       <Link to='/'>Home</Link>
