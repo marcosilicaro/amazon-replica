b94ba7352cdc5e89945bf49b95f711c833089804
marcosilicaro <marcosilicaro@gmail.com>
2020-09-16 17:56:38 -0300

    product component

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Home.css'
+import Product from './Product'
 
 function Home() {
   return (
   <div className='home__banner'>
-     <img 
+      <img 
         src='https://i.imgur.com/KWztOQO.png'
         alt='banner'/>
+      <Product/>
   </div>
   );
 }
---
+++ src/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Product.css'
+
+function Product() {
+    return (
+        <div className='product__container'>
+            Product
+        </div>
+    );
+}
+
+export default Product;
