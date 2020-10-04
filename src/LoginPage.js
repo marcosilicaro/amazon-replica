459eacf5c1230de9ec49f273f22e8bf0850b6f6e
marcosilicaro <marcosilicaro@gmail.com>
2020-10-04 17:40:07 -0300

    login styling

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./LoginPage.css";
 
-function LoginPage() {
+
+const LoginPage = () => {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value)
+  }
+
   return (
-  <div>
-    LoginPage
-  </div>
+    <div className='login'>
+      <div className='login__container'>
+        <img
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1280px-Amazon_logo.svg.png'
+          className='login__logo'
+        />
+        <div className='login__form'>
+          <form>
+            <label>Email: <input type='text' onChange={handleChangeEmail} value={email} /></label>
+            <label>Password: <input type='text' onChange={handleChangePassword} value={password} /></label>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
